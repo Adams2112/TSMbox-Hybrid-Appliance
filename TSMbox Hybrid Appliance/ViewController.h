6676827d9df4ae9e9f9c031f75bf082189862f66
Adam <Adam@Mac-Mini.local>
2015-01-25 22:36:12 +0100

    Initial Commit

---
+++ TSMbox Hybrid Appliance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TSMbox Hybrid Appliance
+//
+//  Created by Adam on 25.01.2015.
+//  Copyright (c) 2015 Infonet Projekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TSMbox Hybrid Appliance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TSMbox Hybrid Appliance
+//
+//  Created by Adam on 25.01.2015.
+//  Copyright (c) 2015 Infonet Projekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
